9fbe747492f2167a6d1b4a30251a14de6bc0f497
Roland Hudak <roland.hudak@hotmail.com>
2018-01-28 16:47:06 +0100

    Fixed few typing errors & created Add account demo

---
+++ src/demo/payment_demo.ts
@@ -0,0 +1,6 @@
+import { PaymentsWidget } from "../main/widgets/payments";
+
+const app = new PaymentsWidget()
+    .setTitle("MyApp")
+    .mount(document.getElementById("app"));
+(self as any).app = app;
