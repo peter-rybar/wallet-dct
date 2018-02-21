44973da3257b6879bc996b220fa16840da2f8d25
Peter Rybar <pr.rybar@gmail.com>
2018-02-21 19:46:33 +0100

    gh-pages

---
+++ sw.js
@@ -0,0 +1,103 @@
+!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o():"function"==typeof define&&define.amd?define(o):o()}(0,function(){"use strict";importScripts("lib/workbox-sw.js"),new WorkboxSW({clientsClaim:!0}).precache([
+  {
+    "url": "assets/css/styles.css",
+    "revision": "62ed320bd408e125218004291087ac12"
+  },
+  {
+    "url": "assets/fonts/MaterialIcons-Regular.woff2",
+    "revision": "a85ccf360fecc9e944dae3617f131577"
+  },
+  {
+    "url": "assets/icons/ic-face-large.png",
+    "revision": "b5f13f5134d4edabf63ab6e2660a2659"
+  },
+  {
+    "url": "assets/icons/ic-face.png",
+    "revision": "283dd0706025f6a685ba39d313fdb958"
+  },
+  {
+    "url": "assets/icons/tmp/bitcoin-07-256.png",
+    "revision": "c5386f08376c0001a2a6279d2194f355"
+  },
+  {
+    "url": "assets/icons/tmp/bitcoin-Blockchain-Crypto-currency_4-512.png",
+    "revision": "35498aa37c80530d329c1f6b557be776"
+  },
+  {
+    "url": "assets/icons/tmp/bitcoin-wallet.png",
+    "revision": "b6de12ea5f7714f9ff0cf34057e2cc0b"
+  },
+  {
+    "url": "assets/icons/tmp/blochain+challenges+icons-03.png",
+    "revision": "ba42ad0306980e8004b3e75550eeb1e3"
+  },
+  {
+    "url": "assets/icons/tmp/Cryptography-512.png",
+    "revision": "8cac8823eb659856b9bbf600b2fc5c16"
+  },
+  {
+    "url": "assets/icons/tmp/electronic_lock-512.png",
+    "revision": "0fc131f3b49018637ba255d185f45688"
+  },
+  {
+    "url": "assets/icons/tmp/electronics-256.png",
+    "revision": "d5222afc45cee197edfd4f5579dc940c"
+  },
+  {
+    "url": "assets/icons/tmp/google-wallet.png",
+    "revision": "998679a50058b86350038cb70085b609"
+  },
+  {
+    "url": "assets/icons/tmp/if_5354_-_Wallet_1313565.png",
+    "revision": "a45476aedf07641504e1746196fc4dc1"
+  },
+  {
+    "url": "assets/icons/tmp/if_GoogleWallet_2613294.png",
+    "revision": "a4928c8ea4742913d1b5ca52b8ce2897"
+  },
+  {
+    "url": "assets/icons/tmp/public_key-512.png",
+    "revision": "b57aa24d9d502980bff91ed2e60cff31"
+  },
+  {
+    "url": "index.html",
+    "revision": "3af868da5419956d479e29d4d947e7f4"
+  },
+  {
+    "url": "index.js",
+    "revision": "c57dc4675933d1eae40128ff78c0d137"
+  },
+  {
+    "url": "lib/dcorejs-lib.js",
+    "revision": "9c37928faa24f12c0460bbddd411e25c"
+  },
+  {
+    "url": "lib/dcorejs.js",
+    "revision": "534655e6cc0313391d17748fd4036008"
+  },
+  {
+    "url": "lib/incremental-dom-min.js",
+    "revision": "d7839c2cb4ac91773e025ee3a67cecc0"
+  },
+  {
+    "url": "lib/store.js",
+    "revision": "539d05a3cd7c6180d7d78548e80165eb"
+  },
+  {
+    "url": "lib/workbox-sw.js",
+    "revision": "685d1ceb6b9a9f94aacf71d6aeef8b51"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "5bc8839b2aba9dd2e1352da7cd4fa9e8"
+  },
+  {
+    "url": "sw.js",
+    "revision": "7b411e57ec6c2316e5d5559f4fb23b49"
+  },
+  {
+    "url": "template.html",
+    "revision": "9b3d2a56fa9c930ebd77be72c2ac4509"
+  }
+])});
+//# sourceMappingURL=sw.js.map
